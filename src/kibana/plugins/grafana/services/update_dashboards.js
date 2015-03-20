f1cf0cf8c9cd4080aff3f4b1ce3e109b9aa201df
lammoth <chelestra2003@yahoo.es>
2015-03-20 14:40:30 +0100

    Working on ES search

--- src/kibana/plugins/grafana/services/update_dashboards.js
+++ src/kibana/plugins/grafana/services/update_dashboards.js
@@ -2,61 +2,43 @@ define(function (require) {
   var module = require('modules').get('app/dashboard');
   var _ = require('lodash');
 
-  // This is the only thing that gets injected into controllers
-  module.service('updateDashboards', function (config, es) {
-    // this.type = SavedDashboard.type;
+  module.service('updateDashboards', function (config, es, $window) {
 
-    // // Returns a single dashboard by ID, should be the name of the dashboard
-    // this.get = function (id) {
+    this.find = function (searchString) {
+      var self = this;
+      var body = searchString ? {
+          query: {
+            simple_query_string: {
+              query: searchString + '*',
+              fields: ['dashboard'],
+              default_operator: 'AND'
+            }
+          }
+        }: { query: {match_all: {}}};
+      return es.search({
+        index: 'grafana-dash',
+        type: 'dashboard',
+        body: body,
+        size: 100
+      })
+      .then(function (resp) {
+        console.log(resp);
+        // return {
+        //   total: resp.hits.total,
+        //   hits: resp.hits.hits.map(function (hit) {
+        //     var source = hit._source;
+        //     source.id = hit._id;
+        //     source.url = self.urlFor(hit._id);
+        //     return source;
+        //   })
+        // };
+      }); 
+    };
 
-    //   // Returns a promise that contains a dashboard which is a subclass of docSource
-    //   return (new SavedDashboard(id)).init();
-    // };
-
-    // this.urlFor = function (id) {
-    //   return kbnUrl.eval('#/dashboard/{{id}}', {id: id});
-    // };
-
-    // this.delete = function (ids) {
-    //   ids = !_.isArray(ids) ? [ids] : ids;
-    //   return Promise.map(ids, function (id) {
-    //     return (new SavedDashboard(id)).delete();
-    //   });
-    // };
-
-
-
-    // this.find = function (searchString) {
-    //   var self = this;
-    //   var body = searchString ? {
-    //       query: {
-    //         simple_query_string: {
-    //           query: searchString + '*',
-    //           fields: ['title^3', 'description'],
-    //           default_operator: 'AND'
-    //         }
-    //       }
-    //     }: { query: {match_all: {}}};
-    //   return es.search({
-    //     index: config.file.kibana_index,
-    //     type: 'dashboard',
-    //     body: body,
-    //     size: 100
-    //   })
-    //   .then(function (resp) {
-    //     return {
-    //       total: resp.hits.total,
-    //       hits: resp.hits.hits.map(function (hit) {
-    //         var source = hit._source;
-    //         source.id = hit._id;
-    //         source.url = self.urlFor(hit._id);
-    //         return source;
-    //       })
-    //     };
-    //   }); 
-    // };
     this.updateGrafanaDash = function (timefilter) {
       console.log(timefilter);
+      this.find({_id:"grafana"});
+      // $window.location.reload();
     };
   });
 });
